d70c13f545b278bf01ab4bd82b5abdf1509a9668
Matheus Alves <theuves@outlook.com>
2018-02-05 00:09:16 -0300

    pequenas alterações

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 (() => {
   "use strict";
 
-  fetch("https://api.github.com/repos/theuves/piii.js/tags").then(response => {
-    response.text().then(json => {
-      const download = JSON.parse(json)[0].zipball_url;
-      const version = JSON.parse(json)[0].name;
-      document.querySelector("#download").href = download;
-      document.querySelector("#version").innerHTML = version;
-    });
-  });
+  const updateElement = (sel, attr, val) =>
+    document.querySelector(sel)[attr] = val;
+
+  const updateEverything = json => {
+    const download = json[0].zipball_url;
+    const version = json[0].name;
+
+    updateElement("#download", "href", download);
+    updateElement("#version", "innerHTML", version);
+  };
+
+  fetch("https://api.github.com/repos/theuves/piii.js/tags")
+    .then(response => response.text())
+    .then(JSON.parse)
+    .then(updateEverything);
 })();
