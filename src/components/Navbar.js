a0136e03b891d61b1ea92d30fc3dfe9e6e2393f4
Matheus Alves <theuves@gmail.com>
2019-01-14 20:26:11 -0300

    Add Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+
+const links = [
+  {
+    title: 'Código-fonte',
+    url: 'https://github.com/piiijs/piii.js',
+  },
+  {
+    title: 'Documentação',
+    url: 'https://github.com/piiijs/piii.js#readme',
+  },
+  {
+    title: 'Download',
+    utl: 'https://github.com/piiijs/piii.js/archive/master.zip'
+  },
+]
+
+const StyledContainer = styled.nav`
+  padding: 15px 0;
+  background-color: black;
+`
+
+const StyledMenu = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  text-align: center;
+`
+
+const StyledMenuItem = styled.li`
+  padding: 0 20px;
+  display: inline-block;
+`
+
+const StyledLink = styled(Link)`
+  font-size: 1.25em;
+  text-decoration: none;
+  color: white;
+`
+
+const Navbar = () => (
+  <StyledContainer>
+    <StyledMenu>
+    {links.map((link, index) => (
+      <StyledMenuItem key={index}>
+        <StyledLink to={link.url}>
+          {link.title}
+        </StyledLink>
+      </StyledMenuItem>
+    ))}
+    </StyledMenu>
+  </StyledContainer>
+)
+
+export default Navbar
