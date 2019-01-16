3dea3b60812979cc34df877154eb425db47847b7
Matheus Alves <theuves@gmail.com>
2019-01-16 09:49:23 -0300

    Remove global styles from index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { createGlobalStyle } from 'styled-components'
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
 import Features from '../components/Features'
@@ -7,16 +6,8 @@ import Installation from '../components/Installation'
 import Footer from '../components/Footer'
 import Layout from '../components/Layout'
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    font-family: 'Segoe UI', Verdana, Arial, sans-serif;
-  }
-`
-
 export default () => (
   <Layout>
-    <GlobalStyle />
     <Header />
     <Navbar />
     <Features />
