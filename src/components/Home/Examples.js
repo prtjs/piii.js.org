edbead1f5b1be2bfeb51f98ca761a373eadba154
Matheus Alves <theuves@gmail.com>
2019-02-25 11:33:10 -0400

    Termina base do site

--- src/components/Home/Examples.js
+++ src/components/Home/Examples.js
@@ -1,26 +1,53 @@
 import React from 'react'
 import styled from 'styled-components'
 
-// Local components
 import Container from '../styled/Container'
 
+// Styles
 const Section = styled.section`
-  padding: 50px 0;
+  margin-bottom: 75px;
+  padding: 50px 0;  
   text-align: center;
 `
 const Title = styled.h1`
   font-size: 2.5em;
+  margin-bottom: 50px;
   line-height: 150%;
 `
+
+// Others components
+const CodePen = () => (
+  <iframe
+    height='600'
+    style={{ width: '100%' }}
+    scrolling='no'
+    title='Chat sem Palavrões (Piii.js)'
+    src='//codepen.io/theuves/embed/YBmPoM/?height=600&theme-id=12945&default-tab=js,result'
+    frameBorder='no'
+  >
+    See the Pen
+    <a href='https://codepen.io/theuves/pen/YBmPoM/'>
+      Chat sem Palavrões (Piii.js)
+    </a>
+    by Matheus Alves
+    (<a href='https://codepen.io/theuves'>@theuves</a>)
+    on
+    <a href='https://codepen.io'>
+      CodePen
+    </a>.
+  </iframe>
+)
 
 const Examples = () => (
   <Section>
     <Container>
       <Title>
         Está criando um sitema para um público infantil ou
-        apenas quer bloquer termos inconvenientes em seu site?
-        Então o Piii.js pode lher ajudar.
+        apenas quer bloquear termos inconvenientes em seu site?
+        Então o Piii.js pode lhe ajudar.
       </Title>
+
+      <CodePen />
     </Container>
   </Section>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,12 @@ import Layout from '../components/Layout'
 // Home components
 import Apresentation from '../components/Home/Apresentation'
 import Examples from '../components/Home/Examples'
+import Footer from '../components/Home/Footer'
 
 export default () => (
   <Layout>
     <Apresentation />
     <Examples />
+    <Footer />
   </Layout>
 )
