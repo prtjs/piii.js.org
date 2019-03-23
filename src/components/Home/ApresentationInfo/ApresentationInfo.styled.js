3202d5128e94899590b5c09207752ffffdb4b517
Matheus Alves <theuves@gmail.com>
2019-03-23 08:34:54 -0400

    Adiciona cor para links simples

--- src/components/Home/ApresentationInfo/ApresentationInfo.styled.js
+++ src/components/Home/ApresentationInfo/ApresentationInfo.styled.js
@@ -3,10 +3,15 @@ import styled from 'styled-components'
 // Design
 import { smMarginY } from '../../design/margins'
 import { mdWidth } from '../../design/devices'
+import { lightBlue } from '../../design/colors'
 
 export const Section = styled.section`
   text-align: center;
   padding: ${smMarginY}px 0;
   box-shadow: 0 2px 1px 0 rgba(0, 0, 0, .25);
   background-color: #ddd;
+
+  a {
+    color: ${lightBlue};
+  }
 `
--- src/components/design/colors.js
+++ src/components/design/colors.js
@@ -1,6 +1,11 @@
+// As cores aqui são da paleta:
+// <https://flatuicolors.com/palette/defo>
+
 export const purple = '#8e44ad'
 
 export const blue = '#5e7083'
+
+export const lightBlue = '#2980b9'
 
 export const orange = '#e67e22'
 
