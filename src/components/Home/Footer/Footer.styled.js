c8c050fec58ffe31c00dcabf2859dcc8cb2c5981
Matheus Alves <theuves@gmail.com>
2019-03-24 13:03:56 -0400

    Footer: Torna-o responsivo

--- src/components/Home/Footer/Footer.styled.js
+++ src/components/Home/Footer/Footer.styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import StyledContainer from '../../styled/StyledContainer'
 
 // Design
+import { smWidth } from '../../design/devices'
 import { mdMarginY, smMarginY } from '../../design/margins'
 
 export const Footer = styled.footer`
@@ -9,24 +10,45 @@ export const Footer = styled.footer`
   color: white;
 `
 export const FooterPrimary = styled.div`
-  padding: ${mdMarginY}px;
+  padding: ${smMarginY}px 0;
+
+  @media (min-width: ${smWidth}px) {
+    padding: ${mdMarginY}px 0;
+  }
 `
 export const FooterSecondary = styled.div`
-  padding: ${smMarginY}px;
+  padding: ${smMarginY}px 0;
   font-size: .85em;
   color: #999;
   background-color: #222;
 
-  // Referente ao Container do último item do footer
+  // Referente ao <Container /> do último item do footer
   > div {
-    justify-content: space-between;
+    > div:not(:last-child) {
+      margin-bottom: 10px;
+    }
+
+    @media (min-width: ${smWidth}px) {
+      justify-content: space-between;
+
+      > div {
+        margin-bottom: 0 !important;
+      }
+    }
   }
 `
 export const Container = styled(StyledContainer)`
-  display: flex;
+  @media (min-width: ${smWidth}px) {
+    display: flex;
+  }
 `
 export const LinksSection = styled.div`
-  flex: 1;
+  margin-bottom: 30px;
+
+  @media (min-width: ${smWidth}px) {
+    margin-bottom: unset;
+    flex: 1;
+  }
 `
 export const LinksTitle = styled.h3`
   margin-bottom: 15px;
